c1d92e215a66c2c9e27854253aa4651e97510f13
temp <temp>
2017-09-18 12:49:53 -0400

    Add project files.

---
+++ Pacman/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace Pacman
+{
+    partial class pacForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.startButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // startButton
+            // 
+            this.startButton.BackColor = System.Drawing.Color.White;
+            this.startButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Aqua;
+            this.startButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Aqua;
+            this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.startButton.Font = new System.Drawing.Font("Segoe UI", 27.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(162, 125);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(174, 92);
+            this.startButton.TabIndex = 0;
+            this.startButton.Text = "Start";
+            this.startButton.UseVisualStyleBackColor = false;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pacForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.startButton);
+            this.Name = "pacForm";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startButton;
+    }
+}
+
---
+++ Pacman/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Pacman
+{
+    public partial class pacForm : Form
+    {
+        public pacForm()
+        {
+            InitializeComponent();
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            //Graphic Setup
+            startButton.Visible = false;
+            Graphics fG = this.CreateGraphics();
+            Pen drawPen = new Pen(Color.Blue, 5);
+            SolidBrush drawBrush = new SolidBrush(Color.Black);
+
+            //path
+            fG.DrawRectangle(drawPen, 0, 30, 300, 100);
+            fG.FillRectangle(drawBrush, 0, 30, 250, 100);
+
+            fG.DrawRectangle(drawPen, 300, 30, 100, 300);
+            fG.FillRectangle(drawBrush, 300, 30, 100, 300);
+
+            drawPen.Color = Color.Black;
+            fG.DrawRectangle(drawPen, 250, 32, 80, 95);
+            fG.FillRectangle(drawBrush, 250, 32, 80, 95);
+
+            fG.DrawRectangle(drawPen, 300, 302, 110, 95);
+            fG.FillRectangle(drawBrush, 300, 302, 110, 95);
+
+            //Pacman
+            Pen pacPen = new Pen(Color.Black);
+            SolidBrush pacBrush = new SolidBrush(Color.Yellow);
+            SolidBrush eraseBrush = new SolidBrush(Color.Black);
+
+            fG.DrawPie(pacPen, 35, 40, 75, 75, 30, 300);
+            fG.FillPie(pacBrush, 35, 40, 75, 75, 30, 300);
+            Thread.Sleep(500);
+
+            fG.DrawEllipse(pacPen, 35, 40, 75, 75);
+            fG.FillEllipse(eraseBrush, 35, 40, 75, 75);
+
+            fG.DrawEllipse(pacPen, 135, 40, 75, 75);
+            fG.FillEllipse(pacBrush, 135, 40, 75, 75);
+            Thread.Sleep(500);
+
+            fG.DrawEllipse(pacPen, 135, 40, 75, 75);
+            fG.FillEllipse(eraseBrush, 135, 40, 75, 75);
+
+            fG.DrawPie(pacPen, 235, 40, 75, 75, 30, 300);
+            fG.FillPie(pacBrush, 235, 40, 75, 75, 30, 300);
+            Thread.Sleep(500);
+
+            fG.DrawEllipse(pacPen, 235, 40, 75, 75);
+            fG.FillEllipse(eraseBrush, 235, 40, 75, 75);
+
+            fG.DrawEllipse(pacPen, 310, 40, 75, 75);
+            fG.FillEllipse(pacBrush, 310, 40, 75, 75);
+            Thread.Sleep(500);
+
+            fG.DrawEllipse(pacPen, 310, 40, 75, 75);
+            fG.FillEllipse(eraseBrush, 310, 40, 75, 75);
+
+            fG.DrawPie(pacPen, 310, 140, 75, 75, 120, 300);
+            fG.FillPie(pacBrush, 310, 140, 75, 75, 120, 300);
+            Thread.Sleep(500);
+
+            fG.DrawEllipse(pacPen, 310, 140, 75, 75);
+            fG.FillEllipse(eraseBrush, 310, 140, 75, 75);
+
+            fG.DrawEllipse(pacPen, 310, 240, 75, 75);
+            fG.FillEllipse(pacBrush, 310, 240, 75, 75);
+
+            //Text
+            Font conFont = new Font("Segoe UI", 24, FontStyle.Bold);
+            SolidBrush conBrush = new SolidBrush(Color.Red);
+            fG.DrawString("Congratulations!", conFont, conBrush, 50, 40);
+
+
+        }
+    }
+}
